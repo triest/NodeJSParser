623a18544e766f11833bd23d9e49dd3c13cec2f5
triest <you@example.com>
2019-09-15 16:01:20 +0300

    Get array of links.

--- parserBody.js
+++ parserBody.js
@@ -1,44 +1,72 @@
-const superagent = require('superagent');
-const DomParser = require('dom-parser');
-const HTMLParser = require('node-html-parser');
+const superagent = require('superagent')
+const DomParser = require('dom-parser')
+const HTMLParser = require('node-html-parser')
 const cheerio = require('cheerio')
+let arrayLinks = Array()
+
+function doRequwest (page) {
+  return new Promise(function (resolve, reject) {
+    superagent.get(
+      'https://www.marathonbet.ru/su/popular/Football?page=' + page +
+      '&pageAction=getPage&_=1568307791615').end((err, res) => {
+      if (!err && res.statusCode == 200) {
+        resolve(res.text)
+      } else {
+        reject(err)
+      }
+    })
+  })
+}
 
 module.exports = {
-    test: async function () {
-        superagent.get('https://www.marathonbet.ru/su/popular/Football?page=1&pageAction=getPage&_=1568307791615')
+  test: async function () {
+    let page = 0
+    //page = page + 1
+    let next = true
+    let parser = new DomParser()
+    let doom
+    let temp
+    let content
+    let doc
+    let data = null
+    let resdata
+    let prop
+    let cheerio
+    let $
+    let links
+    next = true
+    while (page < 30) {
+      page = page + 1
 
-            .end((err, res) => {
-                let data = res.text;
-                try {
-                    const resdata = JSON.parse(data)
-                    let prop = resdata[1].prop;
-                    let content = resdata[0].content;
-                    var parser = new DomParser();
-                    let doom = parser.parseFromString(content);
-                    let temp = parser.parseFromString(doom.rawHTML);
-                    temp = temp.rawHTML;
-                    let doc = parser.parseFromString(temp, "text/xml");
-                    //    console.log(doc.rawHTML);
-                    temp = doc.rawHTML;
-                    let cheerio = require('cheerio');
-                    let $ = cheerio.load(temp);
-                    let links = $('.member-link');
-                    console.log(links['0'].attribs.href);
+      try {
+        data = await doRequwest(page)
+        resdata = JSON.parse(data)
+        prop = resdata[1].val
+        next = prop
+        if (next === false) {
+          break
+        }
+        content = resdata[0].content
 
+        doom = parser.parseFromString(content)
+        temp = parser.parseFromString(doom.rawHTML)
+        temp = temp.rawHTML
+        doc = parser.parseFromString(temp, 'text/xml')
+        //    console.log(doc.rawHTML);
+        temp = doc.rawHTML
+        cheerio = require('cheerio')
+        $ = cheerio.load(temp)
+        links = $('.member-link')
+        for (let i = 0; i < links.length; i++) {
+          arrayLinks.push(links[i].attribs.href)
+        }
 
-                    //   const $ = cheerio.load(temp)
-                    //     console.log($);
+      } catch (err) {
+        console.error(err)
+      }
 
-                    // temp=HTMLParser.parse(temp);
-                    // console.log(temp.childNodes);
-                    //       console.log(doc.rawHTML.getElementsByClassName("member-link"));
-                    //  console.log(temp.getElementsByClassName("member-link")[0]);
-                    //  console.(doom.getElementsByClassName("member-link"))
-
-
-                } catch (err) {
-                    console.error(err)
-                }
-            });
+      //     console.log(arrayLinks)
     }
+    console.log(arrayLinks.length)
+  },
 }
